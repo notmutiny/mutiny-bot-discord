420be34c7a995e8e9d7024dec28e672f682d0295
mutiny <notmutiny@users.noreply.github.com>
2018-09-03 09:30:07 +0000

    Update help.js

--- nm_modules/help.js
+++ nm_modules/help.js
@@ -1,4 +1,4 @@
-var footer = { 
+let footer = { 
     0: {
         level: "You're a guest.",
         fluff: ["Some features are restricted!", "Uh oh, stranger danger!", "Try combining help with a command!"]},
@@ -11,9 +11,10 @@ var footer = {
     },
 }
 
-var requireStrings = {
+let requireStrings = {
     mention: "user mention",
-    textInput: "text input"
+    textInput: "text input",
+    command: "command use"
 }
 
 module.exports = {
@@ -22,33 +23,45 @@ module.exports = {
         return "Get assistance with " + nm.bot.user.username + "'s commands";
     },
 
-    function: async function(request, argument, message, nm) {
+    function: async function(request, argument, message, nm, fs, prefs) {
+        let array = message.content.split(" "),
+            index = array.indexOf(request.command);
+            // rescan commands to force modularity
+
+        if (index > -1) {
+            array.splice(index, 1);
+            message.content = array.join(" ");
+            // remove help to check if a target was provided
+            let result = nm.core.request(message, nm, prefs);
+            if (result) request = result; // push new command
+        }
+
         // embed template data for result discord message
-        var header = "Here's an overview of my commands",
+        let header = "Here's an overview of my commands",
             title = "Made with the https://discord.js.org/ library",
             value = "", thumbnail = nm[request.key].picture;
         
-        if (request.key == "help") { // generate default help message
+        if (request.key == "help") { // generate default message
             value = await generateCategoryDisplay(request, nm) + 
                 "\n\n**Did you know?**\n" +
                 ". . . You can get my attention with `nm` or <@318244733975658496>\n" +
                 ". . . You can lookup commands by typing ` help ` before them\n" +
                 ". . . You can use arguments with commands or by themselves\n\n";
         } else {
-            // replace embed data with specified command data
+            // replace embed data with specified target data
             header = "Here's what I know about **" + (request.command ? request.command : request.argument.command) + "**";          
             title = await getSummary(nm[request.key], nm);
             value = "**Commands** : " + getCommands(nm[request.key]) + "\n\n" +
                     "**Examples** : " + generateExampleCommands(request.key, nm) + "\n\n";
             
-            var requirements = generateRequirements(nm[request.key], nm, true);
+            let requirements = generateRequirements(nm[request.key], nm);
             if (requirements) value += "**Requires** : " + requirements + "\n\n";
             value += await generateArgumentDisplay(request, nm);
         } 
 
         // generate footer message with users stored level and additional flavor text then amend it to embedded value
-        value += "**" + footer[request.account].level + "** " + nm.core.randomElement(footer[request.account].fluff);
-        var embed = nm.core.embed(header, title, value, thumbnail);        
+        value += "**" + footer[request.author.account].level + "** " + nm.core.randomElement(footer[request.author.account].fluff);
+        let embed = nm.core.embed(header, title, value, thumbnail);        
         nm.core.speak(embed, message);
     },
 };
@@ -65,7 +78,7 @@ async function getSummary(object, nm) {
     if (object.summary) {
         if (typeof object.summary == "string") return object.summary;
         else {
-            var res = await object.summary(nm);
+            let res = await object.summary(nm);
             return res;
         }
     } else return "No summary has been set for this command";
@@ -81,7 +94,7 @@ function getThumbnail(object) {
 
 // bot appearance is everything amiright
 function capitalizeFirstLetter(string) {
-    var words = string.split(" ");
+    let words = string.split(" ");
 
     words.forEach(function(word, i) {
         words[i] = word.charAt(0).toUpperCase() + word.slice(1);
@@ -92,33 +105,33 @@ function capitalizeFirstLetter(string) {
 
 // get formatted string of each detected argument
 async function generateArgumentDisplay(request, nm) {
-    var commandRequirement = nm[request.key].require ?
+    let commandRequirement = nm[request.key].require ?
         (nm[request.key].require.account || 0) : 0;
         // get required account level for command
     
-    var commandLocked = commandRequirement > request.account,
+    let commandLocked = commandRequirement > request.author.account,
         result = commandLocked ? ":lock: **This command category is locked**\n\n" : "";
 
     // total amount of arguments in commands category
-    var amount = nm[request.key].arguments ?
+    let amount = nm[request.key].arguments ?
         Object.keys(nm[request.key].arguments).length : 0;
 
     if (amount < 1) return result;
 
-    var locked = [],    // all locked arguments
+    let locked = [],    // all locked arguments
         unlocked = [];  // all unlocked arguments
 
-    for (var a in nm[request.key].arguments) {
-        var argument = nm[request.key].arguments[a], // reference to specific argument in request object
+    for (let a in nm[request.key].arguments) {
+        let argument = nm[request.key].arguments[a], // reference to specific argument in request object
             summary = await getSummary(argument, nm),
             element = "**" + capitalizeFirstLetter(a) + "** : " + getCommands(argument) + "\n" + summary;
 
-        var requirements = generateRequirements(argument, nm);
+        let requirements = generateRequirements(argument, nm);
         if (requirements) element += "\n**Requires** : " + requirements;
 
-        var argumentLocked =  // flag if argument is locked so the next code is legible
-            (argument.require ? (argument.require.account || 0) : 0) > request.account;
-
+        let argumentLocked = // flag if argument is locked so the next code is legible
+            (argument.require ? (argument.require.account || 0) : 0) > request.author.account;
+        
         if (commandLocked || argumentLocked)
             locked.push(element)
         else 
@@ -140,18 +153,18 @@ async function generateArgumentDisplay(request, nm) {
 
 // get formatted string of each detected category
 async function generateCategoryDisplay(request, nm) {
-    var categories = [];
+    let categories = [];
 
-    for (var key in nm) {
-        var script = nm[key]; 
+    for (let key in nm) {
+        let script = nm[key]; 
 
         // only pull from categories with commands
         if (script.commands || script.arguments) {
-            var command = script.commands ?
+            let command = script.commands ?
                 "`[ " + script.commands[0] + " ]`" : "`{ " + key + " }`";
                 // change brackets if theres no available commands to use
 
-            var summary = await getSummary(script, nm);
+            let summary = await getSummary(script, nm);
             categories.push(command + " " + summary);
             // [ discord ] does stuff omg wow so cool
         }
@@ -164,10 +177,10 @@ async function generateCategoryDisplay(request, nm) {
 
 // todo: ensure no duplicate results
 function generateExampleCommands(key, nm) {
-    var object = nm[key];
+    let object = nm[key];
 
-    var commands = [];
-    var argument = [];
+    let commands = [];
+    let argument = [];
 
     if (object.commands) commands = [
         nm.core.randomElement(nm[key].commands), 
@@ -175,7 +188,7 @@ function generateExampleCommands(key, nm) {
     ]
 
     if (object.arguments) {
-        var keys = Object.keys(object.arguments);
+        let keys = Object.keys(object.arguments);
         if (keys.length > 0) {
             argument = [ // pull two random arguments from current command category
                 nm.core.randomElement(object.arguments[keys[nm.core.random(0, keys.length)]].commands),
@@ -195,17 +208,17 @@ function generateExampleCommands(key, nm) {
 function generateRequirements(object, nm, includeAccount) {
     if (!object || !object.require) return; // null checks
 
-    var requirements = nm.core.getRequirements(object),
+    let requirements = nm.core.getRequirements(object),
         result = []; // loop through set requirements
 
-    for (var r in requirements) {
-        var requirement = requirements[r];
+    for (let r in requirements) {
+        let requirement = requirements[r];
         if (requirement) {
             if (r == "account") {
                 if (includeAccount) // special flag so requirements don't go everywhere
                     result.push("`bot " + nm.auth.titles[object.require.account] + "`")
             } else {
-                var string = requireStrings[r] ? requireStrings[r] : r;
+                let string = requireStrings[r] ? requireStrings[r] : r;
                 result.push("`" + string + "`"); // try formatted title
             }
         }
