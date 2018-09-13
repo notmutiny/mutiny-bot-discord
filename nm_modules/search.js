65c8f4b74253ed913ad92c3cb4376a411b4b3e0a
mutiny <notmutiny@users.noreply.github.com>
2018-09-13 00:02:21 +0000

    Update search.js

--- nm_modules/search.js
+++ nm_modules/search.js
@@ -1,44 +1,82 @@
-module.exports = function(googleSearch) {
+const key = "private key pls no steal";
+const cx = "private key pls no steal";
+
+let errors = {
+    limit: {
+        main: function() { return ["daily search limit has been met"] },
+        end: ["and Google denied your request", "and search has been aborted", "so try again tomorrow"]
+    }
+}
+
+module.exports = function(googleImages, googleSearch) {
     module.commands = ["search", "find", "google"];
     module.picture = "https://i.imgur.com/boRehwg.png";
     module.summary = "Fetch requested internet search results";
     module.require = { account: 1, textInput: true };
 
-    const search = new googleSearch({
-        key: "private key pls no steal",
-        cx: "private key pls no steal"
-    });
+    module.arguments = {
+        "images": {
+            commands: ["image", "images", "picture", "pictures"],
+            summary: "Gets a random image result for search query",
+        }
+    };
+
+    const images = new googleImages(cx, key);
+    const search = new googleSearch({cx: cx, key: key});
 
     module.function = function(request, argument, message, nm) {
-        let words = message.content.split(" "),
-            query = words.slice(words.indexOf(request.command) + 1, words.length);
-        if (query.length < 1) return;
-        
-        query = query.join(" ");
-        search.build({
-            q: query,
-            num: 1, 
-        }, function(error, response) {
+        let query = nm.core.cleanQuery(message.content, request, (argument ? ["of"] : null));        
+        if (!query) return;
 
-            if (response.items) {
-                let header = "Here's what I found for **" + query + "**",
-                    title = cleanTitle(response.items[0].title),
-                    value = cleanValue(response.items[0].snippet) + "\n\n" + response.items[0].link;
-                    thumb = (response.items[0].pagemap && response.items[0].pagemap.cse_thumbnail) ?
-                        response.items[0].pagemap.cse_thumbnail[0].src : module.exports.picture;
+        switch (argument) {
+            case "images":
+                images.search(query, {safe: "high"}).then(response => {
+                    response.slice(0, 5); // response not ordered
+                    // response.slice(0, response.length * .75);
+                    let item = nm.core.randomElement(response);
+                    // randomize response to enhance usage
 
-                let embed = nm.core.embed(header, title, value, thumb);
-                nm.core.speak(embed, message);
-            }
+                    let embed = nm.core.embed(
+                        "Here's a random " + (request.argument.command.substring(request.argument.command.length - 1, request.argument.command.length) != "s" ? 
+                            request.argument.command : request.argument.command.substring(0, request.argument.command.length - 1)) + " for **" + query + "**",
+                        cleanTitle(item.description),
+                        item.parentPage,
+                        item.thumbnail.url
+                    );
 
-            else if (!response.items)
-                Speak("I couldn't find any results for " + searchTerm, message); 
+                    // attach full-size image to bottom of embed
+                    if (!embed.embed.image) embed.embed.image = {};                    
+                    embed.embed.image.url = item.url;
 
-            else if (response.error && response.error.errors)
-                response.error.errors.forEach(function(err) {
-                    console.log("Error: "+err.reason+". Search aborted!")
-                })
-        })
+                    nm.core.speak(embed, message);
+                }).catch(error => {
+                    if (error.statusCode == "403")
+                        nm.core.error(request, message, errors.limit);
+                });
+                break;
+
+            default:
+                search.build({ q: query, num: 1 }, function(error, response) {
+
+                    if (response.items) {
+                        let header = "Here's what I found for **" + query + "**",
+                            title = cleanTitle(response.items[0].title),
+                            value = cleanValue(response.items[0].snippet) + "\n\n" + response.items[0].link;
+                            thumb = (response.items[0].pagemap && response.items[0].pagemap.cse_thumbnail) ?
+                                response.items[0].pagemap.cse_thumbnail[0].src : module.exports.picture;
+
+                        let embed = nm.core.embed(header, title, value, thumb);
+                        nm.core.speak(embed, message);
+                    }
+
+                    else if (response.error && response.error.errors)
+                        nm.core.error(request, message, errors.limit);
+
+                    else if (!response.items)
+                        nm.core.speak("I couldn't find any results for " + query, message); 
+                });
+                break;
+        }
     }
 
     return module;
